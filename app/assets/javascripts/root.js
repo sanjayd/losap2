ddf487f552e99b922f514207dc2076bcee050fc3
Sanjay Deshmukh <sdeshmukh@hexiscyber.com>
2014-04-13 16:30:52 -0500

    Display member table from static data

--- app/assets/javascripts/root.js
+++ app/assets/javascripts/root.js
@@ -41,12 +41,6 @@ angular.module('losap').controller('WelcomeController', ['$scope', '$location', 
   };
 }]);
 
-angular.module('losap').filter('displayMonth', function() {
-  return function(month) {
-    return month.format('MMMM YYYY');
-  }
-});
-
 angular.module('losap').directive('lpMonthControl', function() {
   return {
     restrict: 'A',
@@ -56,7 +50,7 @@ angular.module('losap').directive('lpMonthControl', function() {
     templateUrl: 'partials/month-control.html',
     link: function(scope, element, attrs) {
       scope.prevMonth = function() {
-        scope.month.subtract(moment.duration(1, 'months'));
+        scope.month = moment(scope.month).subtract(moment.duration(1, 'months')).toDate();
       };
       
       scope.hasNextMonth = function() {
@@ -64,7 +58,7 @@ angular.module('losap').directive('lpMonthControl', function() {
       };
       
       scope.nextMonth = function() {
-        scope.month.add(moment.duration(1, 'months'));
+        scope.month = moment(scope.month).add(moment.duration(1, 'months')).toDate();
       }
     }
   };
@@ -74,8 +68,84 @@ angular.module('losap').controller('MemberController', ['$scope', '$routeParams'
   function($scope, $routeParams, $location, MemberService) {
   'use strict';
   
+  $scope.stationTimeData = {};
+  $scope.stationTimeData[moment('2014-04-01').toDate()] =[
+    {
+      id: 1,
+      date: '2014-04-11',
+      apparatus: 'Truck',
+      deleted: true
+    },
+    {
+      id: 2,
+      date: '2014-04-12',
+      startTime: '0900',
+      endTime: '1500',
+      deleted: false
+    },
+    {
+      id: 3,
+      date: '2014-04-16',
+      apparatus: 'Engine',
+      deleted: false
+    },
+    {
+      id: 7,
+      date: '2014-04-09',
+      startTime: '1700',
+      endTime: '2100',
+      deleted: true
+    }
+  ];
+  $scope.stationTimeData[moment('2014-03-01').toDate()] = [
+    {
+      id: 4,
+      date: '2014-03-01',
+      apparatus: 'Truck',
+      deleted: true
+    },
+    {
+      id: 5,
+      date: '2014-03-02',
+      startTime: '0900',
+      endTime: '1500',
+      deleted: false
+    },
+    {
+      id: 6,
+      date: '2014-03-06',
+      apparatus: 'Engine',
+      deleted: false
+    },
+    {
+      id: 8,
+      date: '2014-03-09',
+      startTime: '1700',
+      endTime: '2100',
+      deleted: true
+    }
+  ];  
+  
   $scope.member = MemberService.get({id: $routeParams.id});
-  $scope.month = moment().startOf('month');
+  $scope.month = moment().startOf('month').toDate();
+    
+  $scope.$watch('month', function() {
+    var stationTimes = undefined;
+    for(var obj in $scope.stationTimeData) {
+      if (obj == $scope.month) {
+        stationTimes = $scope.stationTimeData[obj];
+      }
+    }
+    $scope.stationTimes = stationTimes;
+  });
+  
+  $scope.deleteStationTime = function(id) {
+    console.debug('deleting: ', id);
+  };
+  
+  $scope.undeleteStationTime = function(id) {
+    console.debug('undeleting: ', id);
+  };
   
   $scope.exit = function() {
     $location.path('/');
